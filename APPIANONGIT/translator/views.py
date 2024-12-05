71a19dd3eb7700c4fa4c8d463f9693350a81321a
CYSTCloud <CYSTCLOUD@gmail.com>
2024-12-05 10:08:33 +0100

    Add files via upload

---
+++ APPIANONGIT/translator/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from translator.models import Translation
+from translator.serializers import TranslationSerializer
+
+# Create your views here.
+
+class FrenchSpanishTranslationViewSet(APIView):
+
+    def get(self, request):
+        return Response(data={'result': "OK"}, status=None)
+    
+    def post(self, request):
+        return Response(data={}, status=None)
+    
+    def put(self, request, pk):
+        return Response(data={}, status=None)
+    
+    def delete(self, request, pk):
+        return Response(data={}, status=None)
+    
+class FrenchEnglishTranslationViewSet(APIView):
+
+    def get(self, request):
+        return Response(data={}, status=None)
+    
+    def post(self, request):
+        return Response(data={}, status=None)
+    
+    def put(self, request, pk):
+        return Response(data={}, status=None)
+    
+    def delete(self, request, pk):
+        return Response(data={}, status=None)
+    
+class AllTranslation(APIView):
+
+    def get(self, request):
+        
+        result = Translation.objects.filter(source_language="FR", target_language="ES")
+        serialized_data = TranslationSerializer(result, many=True)
+
+
+        return Response(data=serialized_data.data, status=status.HTTP_200_OK)
+
+
+def index(request):
+    return render(request, 'index.html', context={})
